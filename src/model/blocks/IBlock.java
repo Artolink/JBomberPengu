166ddbfb6e6d89f1780c4f0bfbaedfcb369b14e1
Emanuele Benedettini <emanuele.benedettini@studio.unibo.it>
2019-04-05 14:55:53 +0200

    add structure

---
+++ src/model/blocks/IBlock.java
@@ -0,0 +1,7 @@
+package model.blocks;
+
+public interface IBlock {
+
+	
+	
+}
---
+++ src/model/player/IEntity.java
@@ -0,0 +1,11 @@
+package model.player;
+
+public interface IEntity {
+
+	public String getName();
+	
+	public void setPoints();
+	
+	public Integer getPoints();
+	
+}
