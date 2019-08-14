45d0a3e8936d7801c5148a405a6ab30d27e1fa05
Naomi <naomi.conzo@studio.unibo.it>
2019-08-14 16:49:48 +0200

    GameController updated with key listener

--- src/view/game/GameController.java
+++ src/view/game/GameController.java
@@ -1,11 +1,16 @@
 package view.game;
 
+import java.io.File;
+import java.util.Map;
+
+import controller.game.Game;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import model.utils.Pair;
 import view.GUIImpl;
 
@@ -17,46 +22,79 @@ public class GameController extends GUIImpl {
     private double blockDimension;
     private double blockSpacing;
     private double borderOffset;
+    private int velocity;
+    private Pair<Integer, Integer> dimensions;
 
     @FXML
     private AnchorPane anchorPane1;
     @FXML
     private Button button;
-    @FXML
-    private GraphicsContext gc;
 
-    private final Canvas canvas = new Canvas();
-    private Pair<Integer, Integer> dimensions;
+    private final Pane canvas = new Pane();
+    private Game game;
+    private Map<Pair<Integer, Integer>, ImageView> blockMap;
+    private ImageView player1;
+    private ImageView player2;
 
     /**
      * Initialize the Game scene.
      */
     public void initialize() {
 
-        //button.setText(getTranslator().getValueOf("give up"));
+        this.game = new Game(this);
+
+        // button.setText(getTranslator().getValueOf("give up"));
 
         anchorPane1.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
                 + "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
 
-        canvas.setWidth(anchorPane1.getPrefWidth());
-        canvas.setHeight(anchorPane1.getPrefHeight() - button.getLayoutY());
-        gc = canvas.getGraphicsContext2D();
+        canvas.prefWidth(anchorPane1.getPrefWidth());
+        canvas.prefHeight(anchorPane1.getPrefHeight() - button.getLayoutY());
         anchorPane1.getChildren().add(canvas);
         // getPage("Game").getScene().width
+
+        this.game.init();
+
+        anchorPane1.setOnKeyPressed((key) -> {
+            final KeyCode code = key.getCode();
+            if (code.equals(KeyCode.UP) || code.equals(KeyCode.DOWN) || code.equals(KeyCode.LEFT)
+                    || code.equals(KeyCode.RIGHT)) {
+                this.game.moveSecondPlayer(code.toString());
+            } else if (code.equals(KeyCode.A) || code.equals(KeyCode.S) || code.equals(KeyCode.D)
+                    || code.equals(KeyCode.W)) {
+                this.game.moveFirstPlayer(code.toString());
+            }
+        });
+
+        anchorPane1.setOnKeyReleased((key) -> {
+            final KeyCode code = key.getCode();
+            if (code.equals(KeyCode.UP) || code.equals(KeyCode.DOWN) || code.equals(KeyCode.LEFT)
+                    || code.equals(KeyCode.RIGHT)) {
+                this.game.stopSecondPlayer(code.toString());
+            } else if (code.equals(KeyCode.A) || code.equals(KeyCode.S) || code.equals(KeyCode.D)
+                    || code.equals(KeyCode.W)) {
+                this.game.stopFirstPlayer(code.toString());
+            }
+        });
 
     }
 
     /**
      * Draw the images on the game area.
      * 
-     * @param image image to draw
-     * @param row   row to add the image
-     * @param col   column to add the image
+     * @param path image to draw
+     * @param row  row to add the image
+     * @param col  column to add the image
      */
-    public void draw(final Image image, final int row, final int col) {
-
-        gc.drawImage(image, blockDimension * row + borderOffset, blockDimension * col + borderOffset,
-                blockDimension - blockSpacing, blockDimension - blockSpacing);
+    public void draw(final String path, final int row, final int col) {
+        if (path != null) {
+            final Image image = new Image(path, blockDimension - blockSpacing, blockDimension - blockSpacing, false,
+                    false);
+            final ImageView view = new ImageView(image);
+            view.relocate(blockDimension * row + borderOffset, blockDimension * col + borderOffset);
+            blockMap.put(new Pair<Integer, Integer>(row, col), view);
+            canvas.getChildren().add(view);
+        }
     }
 
     /**
@@ -69,8 +107,8 @@ public class GameController extends GUIImpl {
 
         anchorPane1.setPrefWidth(width);
         anchorPane1.setPrefHeight(height);
-        canvas.setWidth(width);
-        canvas.setHeight(height - button.getLayoutY());
+        canvas.prefWidth(width);
+        canvas.prefHeight(height);
     }
 
     /**
@@ -103,7 +141,6 @@ public class GameController extends GUIImpl {
      */
     @Override
     public Pair<Double, Double> getSizes() {
-
         return new Pair<Double, Double>(canvas.getWidth(), canvas.getHeight());
     }
 
@@ -142,11 +179,10 @@ public class GameController extends GUIImpl {
         double computeX = 0;
         double computeY = 0;
 
-        computeX = blockDimension * row;
-        computeY = blockDimension * col;
+        computeX = blockDimension * row + 2 * borderOffset;
+        computeY = blockDimension * col + 2 * borderOffset;
         return new Pair<Double, Double>(computeX, computeY);
     }
-
 
     /**
      * An event occurs when the button is pressed.
@@ -154,5 +190,82 @@ public class GameController extends GUIImpl {
     @FXML
     public void buttonPressed() {
         loadPage("MainMenu");
+    }
+
+    /**
+     * It draws the players on the start point.
+     */
+    public void drawPlayers() {
+        final Image imagePlayer1 = new Image(
+                ClassLoader.getSystemClassLoader().getResource("view") + File.separator + "bomba gialla.png",
+                blockDimension - blockSpacing, blockDimension - blockSpacing, false, false);
+        this.player1 = new ImageView(imagePlayer1);
+        this.player1.relocate(borderOffset, borderOffset);
+        canvas.getChildren().add(this.player1);
+
+        final Image imagePlayer2 = new Image(
+                ClassLoader.getSystemClassLoader().getResource("view") + File.separator + "bomba rossa.png",
+                blockDimension - blockSpacing, blockDimension - blockSpacing, false, false);
+        this.player2 = new ImageView(imagePlayer2);
+        this.player2.relocate(blockDimension * (dimensions.getX() - 1) + borderOffset,
+                blockDimension * (dimensions.getX() - 1) + borderOffset);
+        canvas.getChildren().add(this.player2);
+    }
+
+    /**
+     * It moves one player.
+     * 
+     * @param direction direction to move
+     */
+    public void moveFirstPlayer(final String direction) {
+        switch (direction) {
+        case "S":
+            player1.relocate(player1.getLayoutX(), player1.getLayoutY() + velocity);
+            break;
+        case "A":
+            player1.relocate(player1.getLayoutX() - velocity, player1.getLayoutY());
+            break;
+        case "D":
+            player1.relocate(player1.getLayoutX() + velocity, player1.getLayoutY());
+            break;
+        case "W":
+            player1.relocate(player1.getLayoutX(), player1.getLayoutY() - velocity);
+            break;
+        default:
+            break;
+        }
+    }
+
+    /**
+     * It moves one player.
+     * 
+     * @param direction direction to move
+     */
+    public void moveSecondPlayer(final String direction) {
+        switch (direction) {
+        case "DOWN":
+            player2.relocate(player2.getLayoutX(), player2.getLayoutY() + velocity);
+            break;
+        case "LEFT":
+            player2.relocate(player2.getLayoutX() - velocity, player2.getLayoutY());
+            break;
+        case "RIGHT":
+            player2.relocate(player2.getLayoutX() + velocity, player2.getLayoutY());
+            break;
+        case "UP":
+            player2.relocate(player2.getLayoutX(), player2.getLayoutY() - velocity);
+            break;
+        default:
+            break;
+        }
+    }
+
+    /**
+     * Setter for velocity.
+     * 
+     * @param velocity velocity
+     */
+    public void setPlayersVelocity(final int velocity) {
+        this.velocity = velocity;
     }
 }
