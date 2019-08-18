4000f4954de6ebabbfa7268ee5f7c296d247350f
Emanuele Benedettini <emanuele.benedettini@studio.unibo.it>
2019-08-19 00:41:29 +0200

    Added winner/loser visualization in view

--- src/view/game/GameEndedController.java
+++ src/view/game/GameEndedController.java
@@ -1,8 +1,11 @@
 package view.game;
 
+import controller.ControllerImpl;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import model.language.ApplicationStrings;
 import view.PageController;
 
@@ -12,18 +15,34 @@ import view.PageController;
 public class GameEndedController extends PageController {
 
     @FXML
-    private Label statusLabel;
+    private Label playerNameLabel1;
+    @FXML
+    private Label playerNameLabel2;
     @FXML
     private Button mpBtn;
     @FXML
     private Button backBtn;
+    @FXML
+    private ImageView playerImage1;
+    @FXML
+    private ImageView playerImage2;
+    
+    
+    private ControllerImpl controller;
 
-    /**
-     * 
-     * @param phrase - The phrase you want to display
-     */
-    public void setLabelText(final String phrase) {
-        statusLabel.setText(phrase);
+    public void initialize() {
+        this.controller = (ControllerImpl) getController();
+        this.controller.GameEnded(this);
+    }
+
+    public void leftPlayerSet(String text, String path) {
+        playerNameLabel1.setText(text);
+        playerImage1.setImage(new Image(path));
+    }
+
+    public void rightPlayerSet(String text, String path) {
+        playerNameLabel2.setText(text);
+        playerImage2.setImage(new Image(path));
     }
 
     /**
